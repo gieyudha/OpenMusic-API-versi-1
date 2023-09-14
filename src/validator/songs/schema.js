a38b79fbe30ca84d6935278233f21aa9d1c6bd6d
Fergie <fergieyudha@gmail.com>
2023-09-14 12:55:16 +0700

    Proyek 1

---
+++ src/validator/songs/schema.js
@@ -0,0 +1,14 @@
+/* eslint-disable newline-per-chained-call */
+const Joi = require('joi');
+
+const currentYear = new Date().getFullYear();
+const SongPayloadSchema = Joi.object({
+  title: Joi.string().required(),
+  year: Joi.number().min(1900).max(currentYear).required(),
+  performer: Joi.string().required(),
+  genre: Joi.string().required(),
+  duration: Joi.number(),
+  albumId: Joi.string(),
+});
+
+module.exports = { SongPayloadSchema };
